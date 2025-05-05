c6c3b8e5efca0941f4b544205e843c4238e3c461
Yousaf Ali Shah <yousaf.shah@veroke.com>
2025-05-05 16:29:35 +0500

    commit for checking github push trigger

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const http = require('http');
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.end('Hello from Node.js inside Docker! Updated');
+  res.end('Hello from Node.js inside Docker! Updated! Another update');
 });
 
 server.listen(port, () => {
